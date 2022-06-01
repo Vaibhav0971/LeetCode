e515107b9ff5e3ec3a078a2d902feafd3c591a9d
Vaibhav Sharma <vaibhav.sharma0971@gmail.com>
2022-06-01 09:57:54 +0530

    Time: 7 ms (9.59%), Space: 6.6 MB (52.18%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        bool flag = false;
+        for(int i=s.length() - 1; i>=0; i--){
+            if(s[i] == ' ' && flag){
+                return count;
+            }
+            if(s[i] != ' '){
+                count++;
+                flag = true;
+            }
+        }
+        return count;
+    }
+};
