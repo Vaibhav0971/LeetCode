b4aa82bea589f9f08d11055d3b2ca1b76f9188d9
Vaibhav Sharma <vaibhav.sharma0971@gmail.com>
2022-07-11 20:26:18 +0530

    Time: 345 ms (9.48%), Space: 7.5 MB (64.37%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int solve(vector<int>& nums, int start, int end){
+        if(start > end){
+            return 0;
+        }
+        
+        int left = nums[start] + min(solve(nums, start+2, end), solve(nums, start+1, end-1));
+        int right = nums[end] + min(solve(nums, start, end-2), solve(nums, start+1, end-1));
+        
+        return max(left, right);
+    }
+    
+    
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        
+        if(nums.size() == 1) return true;
+        
+        int ans = solve(nums, 0, nums.size()-1);
+        
+        int sum = 0;
+        for(auto i : nums)
+            sum += i;
+        
+        return ans >= (sum - ans); 
+    }
+};
