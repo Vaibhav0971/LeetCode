527b659fc968eebd0a5525b2c755ded8df141c7d
Vaibhav Sharma <vaibhav.sharma0971@gmail.com>
2022-07-13 22:28:34 +0530

    Time: 497 ms (56.70%), Space: 7.8 MB (91.48%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,51 +1,36 @@
 class Solution {
     
-    bool isSafe(int x, int y, int index, vector<vector<char>>& board, string& word){
-        if((x >= 0 && x < board.size()) && (y >= 0 && y < board[0].size()) && board[x][y] != '-' && board[x][y] == word[index]){
-            return true;
-        }        
+bool solve(int row, int col, int index, vector<vector<char>>& board, string& word){
+
+    if((row < 0 || row >= board.size()) || (col < 0 || col >= board[0].size()) || (board[row][col] == '-') || (board[row][col] != word[index])){
         return false;
-    }
-    
-    
-    bool solve(int row, int col, int index, vector<vector<char>>& board, string& word){
-        if(index >= word.length() - 1) return true;
-        
-        char temp = board[row][col];
-        board[row][col] = '-';
-        
-        // move down
-        if(isSafe(row+1, col, index+1, board, word)){
-            if(solve(row + 1, col, index+1, board, word))
-                return true;
-        }
-        
-        // move left
-        if(isSafe(row, col-1, index+1, board, word)){
-            if(solve(row, col - 1, index+1, board, word))
-                return true;
-        }
-        
-        // move right
-        if(isSafe(row, col+1, index+1, board, word)){
-            if(solve(row, col + 1, index+1, board, word))
-                return true;
-        }
-        
-        // move up
-        if(isSafe(row-1, col, index+1, board, word)){
-            if(solve(row - 1, col, index+1, board, word))
-                return true;
-        }
-        
-        board[row][col] = temp;
-        
-        return false;
-    }
+    }       
+
+    if(index == word.length() - 1) return true;
+
+    char temp = board[row][col];
+    board[row][col] = '-';
+
+    // move down
+    bool down = solve(row + 1, col, index+1, board, word);
+
+    // move left
+    bool left = solve(row, col - 1, index+1, board, word);
+
+    // move right
+    bool right = solve(row, col + 1, index+1, board, word);
+
+    // move up
+    bool up = solve(row - 1, col, index+1, board, word);
+
+    board[row][col] = temp;
+
+    return left || right || up || down;
+}
     
     
 public:
-    bool exist(vector<vector<char>>& board, string word) {
+    bool exist(vector<vector<char>>& board, string word) {        
         
         for(int i=0; i<board.size(); i++){
             for(int j=0; j<board[i].size(); j++){
