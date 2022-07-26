be0a58adc4e4a631d9c071a5c3410ce6e01511ac
Vaibhav Sharma <vaibhav.sharma0971@gmail.com>
2022-07-27 04:05:14 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.84%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -16,7 +16,7 @@ class Solution {
 public:
     int countGoodNumbers(long long n) {
         long long even = (n/2)+(n%2); // no. of even index
-        long long odd = n/2;          // no. of odd index
+        long long odd = n/2;          // no. of odd index 
         long long result = (power(5,even)*power(4,odd))%mod;
         return result;
     }
