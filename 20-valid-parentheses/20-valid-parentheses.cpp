dbc238f525d8b659d5bc67ffbd9647103b5f67fc
Vaibhav Sharma <vaibhav.sharma0971@gmail.com>
2022-07-30 01:14:05 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (79.90%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -3,6 +3,8 @@ public:
     bool isValid(string s) {
         stack<char> st;
         
+        if(s.length()%1) return false;
+        
         for(int i=0; i<s.length(); i++){
             if(s[i] == '[' || s[i] == '{' || s[i] == '('){
                 st.push(s[i]);
