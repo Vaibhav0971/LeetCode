a6dc793af7cf6ed2d559e92d7469e943e45353e0
Vaibhav Sharma <vaibhav.sharma0971@gmail.com>
2022-09-19 22:55:28 +0530

    Time: 655 ms (26.12%), Space: 119 MB (8.31%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -0,0 +1,64 @@
+class MedianFinder {
+public:
+    priority_queue<double> maxHeap;
+    priority_queue<double, vector<double>, greater<double>> minHeap;
+    double median;
+    
+    MedianFinder() {
+        median = 0;
+    }
+    
+    int signum(int a, int b){
+        return a - b;
+    }
+    
+    void addNum(double num) {
+        int sig = signum(maxHeap.size(), minHeap.size());
+        
+        if(num < median){
+            // push in max heap
+            if(sig == 0){
+                maxHeap.push(num);  
+                median = maxHeap.top();
+            }
+            else if(sig == -1){
+                maxHeap.push(num);  
+                median = (maxHeap.top() + minHeap.top()) / 2;
+            }
+            else if(sig == 1){
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+                maxHeap.push(num);
+                median = (maxHeap.top() + minHeap.top()) / 2;
+            }            
+        }
+        else{
+            // push in min heap
+            if(sig == 0){
+                minHeap.push(num);    
+                median = minHeap.top();
+            }
+            else if(sig == 1){
+                minHeap.push(num);    
+                median = (maxHeap.top() + minHeap.top()) / 2;                
+            }
+            else if(sig == -1){
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+                minHeap.push(num);
+                median = (maxHeap.top() + minHeap.top()) / 2;   
+            }
+        }
+    }
+    
+    double findMedian() {
+        return median;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
